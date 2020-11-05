0557d7e961e60fa91fb57de0c7ffb967751f3d1c
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-11-05 16:34:19 -0500

    capitalize ability in css, clean and fix Question object, create child QuestionWithDiagram object

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // a correct amount of dummy answers
 
     })
+
 })
 
 // End of Dom Loaded
@@ -19,23 +20,54 @@ function loadForm(){
 
     const form = document.createElement("FORM");
           form.class = "add-question-form";
+    
+    const keys = ["question", "answer", "dummy"]
+    
+    
+    
+    
+          makeLabeledInput = (k) => {
+    let node = document.createElement("P");
+    let inputLabel = document.createElement("LABEL");
+        inputLabel.setAttribute("value", `${k}:`);
+        inputLabel.innerText=`${k}: `;
+        inputLabel.setAttribute("class", "capitalize");
+        inputLabel.setAttribute("for", `${k}`)
+        
+    let inputText = document.createElement("INPUT")
+        inputText.setAttribute("name", `${k}`);
+        inputText.setAttribute("id", `${k}`);
+        inputText.setAttribute("type", "text");
+    
+    node.appendChild(inputLabel);
+    node.appendChild(inputText);
 
+    return document.body.appendChild(node);
+    }
+     // Create an input element for emailID 
+     // var EID = document.createElement("input"); 
+     // EID.setAttribute("type", "text"); 
+     // EID.setAttribute("name", "emailID"); 
+     // EID.setAttribute("placeholder", "E-Mail ID"); 
+  
+
+    
 }
 
 // classes
 
 class Question {
-    constructor(question, answer, dummy=[], diagram_info=nil){
+    constructor(question, answer, dummy=[]){
         this.question = question;
         this.answer = answer;
-        this.diagram_info = diagram_info;
         this.dummy = dummy;
     }
 
     pickDummies(requiredNumber=3){
         const dummyQuestions = [];
         //let questionPossibilities = Object.assign({}, this.dummy)
-        //try making a list, to remove random questions from so that it takes less work
+        //try making a list, to remove random questions from so that it takes less work.
+        //can combine splice with indexOf to remove random items
         let randomNum;
 
         while(dummyQuestions.length < requiredNumber){
@@ -52,4 +84,16 @@ class Question {
         return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
       }
 }
+class QuestionWithDiagram extends Question{
+    constructor(question, answer, dummy=[], diagram_info=false){
+        super(question);
+        super(answer);
+        super(dummy);
+        this.diagram_info = diagram_info;
+    }
+}
 
+//blank question -- do i even need it!?
+const blank = new Question(
+    "If the population of bobcats decreases, what will most likely be the long-term effect on the rabbit population?", "It will increase and then decrease.",
+["It will increase, only.", "It will decrease, only.", "It will decrease and then increase."])
