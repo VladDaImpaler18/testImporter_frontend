abd17a8adce6b7af5ff03813262d6e2436596912
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-11-04 23:28:22 -0500

    pickDummies and getRandomDummies methods done for Question Object. Optimization needed

--- index.js
+++ index.js
@@ -32,8 +32,24 @@ class Question {
         this.dummy = dummy;
     }
 
-    pickDummies(number=3){
+    pickDummies(requiredNumber=3){
+        const dummyQuestions = [];
+        //let questionPossibilities = Object.assign({}, this.dummy)
+        //try making a list, to remove random questions from so that it takes less work
+        let randomNum;
 
+        while(dummyQuestions.length < requiredNumber){
+            //as is this if there are the minumum of 3 dummy questions, the while loop CAN run more than 3 times to pick up the questions
+            randomNum = this.getRandomDummies;
+            if(!dummyQuestions.includes(this.dummy[randomNum])){ dummyQuestions << randomNum }
+        }
+        return dummyQuestions;
+        
     }
+    getRandomDummies(){
+        let min = Math.ceil(1);
+        let max = Math.floor(this.dummy.length);
+        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+      }
 }
 
