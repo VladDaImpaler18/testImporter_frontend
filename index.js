46183dc8de5e1950b33fb16fec368a390daafad0
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-11-06 04:56:19 -0500

    Question's pickDummies method fix and made effective. Now picks questions from list, and remove that question to avoid duplicates and less iteration, error check

--- index.js
+++ index.js
@@ -16,42 +16,36 @@ document.addEventListener("DOMContentLoaded", () => {
 function loadForm(){
     const div = document.createElement("DIV");
           div.class = "formContainer";
-        //same as div.setAttribute('id', "formContainer");
 
     const form = document.createElement("FORM");
           form.class = "add-question-form";
     
-    const keys = ["question", "answer", "dummy"]
+    const formItems = ["question", "answer", "dummy"]
+    //create part to upload diagram
+    //create that part with eventListener that makes a Labeled Input for the diagram_info
+    //if param[diagram_info] && object.diagram.attached? Then make the object in javascript using the right class
     
     
     
-    
-          makeLabeledInput = (k) => {
-    let node = document.createElement("P");
-    let inputLabel = document.createElement("LABEL");
-        inputLabel.setAttribute("value", `${k}:`);
-        inputLabel.innerText=`${k}: `;
-        inputLabel.setAttribute("class", "capitalize");
-        inputLabel.setAttribute("for", `${k}`)
+    makeLabeledInput = (k) => {
+        let node = document.createElement("P");
+        let inputLabel = document.createElement("LABEL");
+            inputLabel.setAttribute("value", `${k}:`);
+            inputLabel.innerText=`${k}: `;
+            inputLabel.setAttribute("class", "capitalize");
+            inputLabel.setAttribute("for", `${k}`)
+            
+        let inputText = document.createElement("INPUT")
+            inputText.setAttribute("name", `${k}`);
+            inputText.setAttribute("id", `${k}`);
+            inputText.setAttribute("type", "text");
         
-    let inputText = document.createElement("INPUT")
-        inputText.setAttribute("name", `${k}`);
-        inputText.setAttribute("id", `${k}`);
-        inputText.setAttribute("type", "text");
-    
-    node.appendChild(inputLabel);
-    node.appendChild(inputText);
+        node.appendChild(inputLabel);
+        node.appendChild(inputText);
 
-    return document.body.appendChild(node);
+        return document.body.appendChild(node);
     }
-     // Create an input element for emailID 
-     // var EID = document.createElement("input"); 
-     // EID.setAttribute("type", "text"); 
-     // EID.setAttribute("name", "emailID"); 
-     // EID.setAttribute("placeholder", "E-Mail ID"); 
-  
-
-    
+   
 }
 
 // classes
@@ -63,26 +57,26 @@ class Question {
         this.dummy = dummy;
     }
 
-    pickDummies(requiredNumber=3){
-        const dummyQuestions = [];
-        //let questionPossibilities = Object.assign({}, this.dummy)
+    pickDummies(requiredNumber){
+        requiredNumber = requiredNumber || 3;
+        const chosenQuestions = [];
+        let questionPool =  [...this.dummy]
+        if(requiredNumber > questionPool.length){break;} //make a throw and catch exception
         //try making a list, to remove random questions from so that it takes less work.
         //can combine splice with indexOf to remove random items
-        let randomNum;
-
-        while(dummyQuestions.length < requiredNumber){
-            //as is this if there are the minumum of 3 dummy questions, the while loop CAN run more than 3 times to pick up the questions
-            randomNum = this.getRandomDummies;
-            if(!dummyQuestions.includes(this.dummy[randomNum])){ dummyQuestions << randomNum }
+        const pickRandomly = (array) => {
+            return array[array.length * Math.random() << 0];
+        };
+        while(chosenQuestions.length < requiredNumber){
+            const randomQ = pickRandomly(questionPool);
+            questionPool.splice(questionPool.indexOf(randomQ),1);
+            chosenQuestions.push(randomQ);
+            console.log(`chosen length = ${chosenQuestions.length}`);
+            console.log(`original array = ${this.dummy.length}`);
         }
-        return dummyQuestions;
+        return chosenQuestions;
         
     }
-    getRandomDummies(){
-        let min = Math.ceil(1);
-        let max = Math.floor(this.dummy.length);
-        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
-      }
 }
 class QuestionWithDiagram extends Question{
     constructor(question, answer, dummy=[], diagram_info=false){
@@ -93,7 +87,7 @@ class QuestionWithDiagram extends Question{
     }
 }
 
-//blank question -- do i even need it!?
+//blank question -- do i even need it!? -- Great for quick testing
 const blank = new Question(
     "If the population of bobcats decreases, what will most likely be the long-term effect on the rabbit population?", "It will increase and then decrease.",
 ["It will increase, only.", "It will decrease, only.", "It will decrease and then increase."])
