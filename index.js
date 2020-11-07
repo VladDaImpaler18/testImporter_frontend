cd064d58cac7f6b6e8c9379e8bb310856b014f13
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-11-07 14:33:19 -0500

    loadForm() initial attempt functional

--- index.js
+++ index.js
@@ -12,40 +12,73 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 // End of Dom Loaded
-
 function loadForm(){
     const div = document.createElement("DIV");
           div.class = "formContainer";
+    document.body.appendChild(div);
 
     const form = document.createElement("FORM");
+          form.id="add-question-form"
           form.class = "add-question-form";
-    
-    const formItems = ["question", "answer", "dummy"]
+    document.body.appendChild(form);
+    const formItems = ["question", "answer", "dummy"] //not a great way but it'll do for now.
     //create part to upload diagram
     //create that part with eventListener that makes a Labeled Input for the diagram_info
     //if param[diagram_info] && object.diagram.attached? Then make the object in javascript using the right class
     
-    
-    
-    makeLabeledInput = (k) => {
-        let node = document.createElement("P");
-        let inputLabel = document.createElement("LABEL");
-            inputLabel.setAttribute("value", `${k}:`);
-            inputLabel.innerText=`${k}: `;
-            inputLabel.setAttribute("class", "capitalize");
-            inputLabel.setAttribute("for", `${k}`)
-            
-        let inputText = document.createElement("INPUT")
-            inputText.setAttribute("name", `${k}`);
-            inputText.setAttribute("id", `${k}`);
-            inputText.setAttribute("type", "text");
-        
-        node.appendChild(inputLabel);
-        node.appendChild(inputText);
+    //diagamInput
+    formItems.forEach( item => makeLabeledInput(item));
 
-        return document.body.appendChild(node);
+    const submitBtn = document.createElement("BUTTON");
+    submitBtn.innerText = "Submit";
+    form.appendChild(submitBtn);
+
+    function makeLabeledInput(item){
+        if(item != "dummy"){
+            let node = document.createElement("P");
+            let inputLabel = document.createElement("LABEL");
+                inputLabel.setAttribute("value", `${item}:`);
+                inputLabel.innerText=`${item}: `;
+                inputLabel.setAttribute("class", "capitalize");
+                inputLabel.setAttribute("for", `${item}`)
+                
+            let inputText = document.createElement("INPUT")
+                inputText.setAttribute("name", `${item}`);
+                inputText.setAttribute("id", `${item}`);
+                inputText.setAttribute("type", "text");
+                
+            node.appendChild(inputLabel);
+            node.appendChild(inputText);
+            form.appendChild(node);
+        }
+        if(item==="dummy"){
+            for(let i=1; i<=3; i++){
+                
+                let node = document.createElement("P");
+                let inputLabel = document.createElement("LABEL");
+                    inputLabel.setAttribute("value", `${item}-${i}:`);
+                    inputLabel.innerText=`${item}-${i}: `;
+                    inputLabel.setAttribute("class", "capitalize");
+                    inputLabel.setAttribute("for", `${item}-${i}`)
+                    
+                let inputText = document.createElement("INPUT")
+                    inputText.setAttribute("name", `${item}-${i}`);
+                    inputText.setAttribute("id", `${item}-${i}`);
+                    inputText.setAttribute("type", "text");
+
+                    node.appendChild(inputLabel);
+                    node.appendChild(inputText);
+                if(i===3){
+                    let plusBtn = document.createElement("BUTTON");
+                        plusBtn.innerText = "+";
+                    node.appendChild(plusBtn);
+                }
+                form.appendChild(node);
+            }
+
+        }
     }
-   
+       
 }
 
 // classes
@@ -61,9 +94,7 @@ class Question {
         requiredNumber = requiredNumber || 3;
         const chosenQuestions = [];
         let questionPool =  [...this.dummy]
-        if(requiredNumber > questionPool.length){break;} //make a throw and catch exception
-        //try making a list, to remove random questions from so that it takes less work.
-        //can combine splice with indexOf to remove random items
+        if(requiredNumber > questionPool.length){ return console.log(`Not enough questions to fulfil ${requiredNumber} questions`);} //make a throw and catch exception
         const pickRandomly = (array) => {
             return array[array.length * Math.random() << 0];
         };
