653f5ad7293c1c6003f75b9bd95d0066b8bf9d62
VladDaImpaler18 <vladDaImpaler@gmail.com>
2020-11-28 02:41:49 -0500

    fixed update bug by correcting find_by(); CRUD functional

--- index.js
+++ index.js
@@ -98,10 +98,8 @@ document.addEventListener("DOMContentLoaded", () => {
             fetch(`http://localhost:3000/questions/`, configObj)
             .then(response => response.json())
             .then(modded => {
-                debugger;
                 const modifiedObj = new Question(modded.question, modded.answer, modded.dummy, modded.category.title, modded.diagram);
-                const originalObj = Question.find_by("question", origQuestion);
-                debugger;
+                const originalObj = Question.find_by("question", origQuestion.question);
                 const indexToUpdate = Question.all.indexOf(originalObj);
                 Question.all.splice(indexToUpdate, 1, modifiedObj)
                 delete originalObj; //does this free up memory?
@@ -264,10 +262,6 @@ function formToParams(form_element, placeholderOption){
     }
 }
 
-function isEqual(obj1, obj2){
-    //code to check if objects keys and values are the same.
-    debugger;
-}
 function loadForm(questionObj){
     clear();
     console.log("Form Loaded");
@@ -354,112 +348,3 @@ function loadForm(questionObj){
 }
 
 navbar();
-/* Legacy code
-function loadForm(){
-    clear();
-
-    const div = document.createElement("DIV");
-          div.class = "formContainer";
-          div.id = "div-for-question";
-    
-    document.body.appendChild(div);
-
-    const form = document.createElement("FORM");
-          form.id="add-question-form";
-          form.setAttribute("autocomplete", "off");
-    div.appendChild(form);
-  
-    const formItems = ["question", "answer", ["dummy", "dummy", "dummy"]]; //not a great way but it'll do for now.
-    
-    const category = document.createElement("DIV");
-          category.setAttribute("class", "autocomplete");
-          category.setAttribute("style", "width:300px;");
-
-    const categoryInput = document.createElement("INPUT");
-          categoryInput.setAttribute("id", "categoryInput");
-          categoryInput.setAttribute("type", "text");
-          categoryInput.setAttribute("name", "myCategory");
-          categoryInput.setAttribute("placeholder", "Category");
-
-    category.appendChild(categoryInput);
-    form.appendChild(category);
-     
-
-
-    formItems.forEach( item => makeAdditionalInput(item) );
-
-    const submitBtn = document.createElement("BUTTON");
-    submitBtn.innerText = "Submit";
-    submitBtn.setAttribute("id", "submitButton");
-    form.appendChild(submitBtn);
-
-    function makeAdditionalInput(item){
-        if(Array.isArray(item)){
-            for(let i=0; i<item.length; i++){ 
-                let row = makeLabeledInput(item[i], i);
-
-                if(i===item.length-1){
-                    
-                    let plusBtn = document.createElement("BUTTON");
-                        //set button identifier for quick find
-                        plusBtn.innerText = "+";
-                        plusBtn.setAttribute("id", "addButton");
-                        plusBtn.addEventListener("click", (e)=> {
-                            let count = document.getElementsByName("dummies").length;
-                            let btn = e.target;
-                            let additionalRow = makeLabeledInput("dummy",count);
-                            let dummyInputs = Object.values(document.getElementsByName("dummies"));
-                            if (dummyInputs.every(input => input.value)){
-                                btn.parentElement.removeChild(btn);
-                                additionalRow.appendChild(btn);
-                                let submitBtn = document.getElementById("submitButton");
-                                form.removeChild(submitBtn);
-                                form.appendChild(additionalRow);
-                                form.appendChild(submitBtn);
-                            }
-                            else{
-                                dummyInputs.forEach( input => {
-                                    if(!input.value){input.placeholder = "Must have input before you can add another"}
-                                })
-                            }
-                        });
-                    row.appendChild(plusBtn);
-                }
-                form.appendChild(row);
-            }
-        }
-        else {
-            let row = makeLabeledInput(item);
-            form.appendChild(row);
-        }
-    }
-
-    function makeLabeledInput(item,number = false){
-        let node = document.createElement("P");
-        let inputLabel = document.createElement("LABEL");
-            inputLabel.setAttribute("value", `${item}:`);
-            inputLabel.setAttribute("class", "capitalize");
-        let inputText = document.createElement("INPUT")
-            inputText.setAttribute("type", "text");
-            if(number) {
-                inputLabel.innerText=`${item}-${number}: `;
-                inputLabel.setAttribute("for", `${item}-${number}`); 
-                inputText.setAttribute("name", `${item}-${number}`);
-                inputText.setAttribute("id", `${item}-${number}`);
-            }
-            else { 
-                inputLabel.innerText=`${item}: `;
-                inputLabel.setAttribute("for", `${item}`);
-                inputText.setAttribute("name", `${item}`);
-                inputText.setAttribute("id", `${item}`);
-    
-            }
-        if(item === "dummy"){ inputText.setAttribute("name", "dummies"); } // document.getElementsByName("dummies") for array of dummies
-            
-        node.appendChild(inputLabel);
-        node.appendChild(inputText);
-        return node;
-    }
-     
-    autocomplete(document.getElementById("categoryInput"), Category.all);
-}*/
